c1bbbc1530cbdedc9035ab47e28f009195a14dfb
dev-nirob1 <dev-nirob1@gmail.com>
2023-08-30 15:31:14 +0600

    testimonials collection added to database

--- index.js
+++ index.js
@@ -23,6 +23,16 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const testimonialsCollection = client.db("toyCar").collection("testimonial");
+
+    app.get('/testimonials', async(req, res) =>{
+        const cursor = testimonialsCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
+
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
